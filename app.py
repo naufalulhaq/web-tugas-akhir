0f8b9db91e4d7a35c3581a7c9c197a8b46f50c13
Naufal Dhiya Ulhaq <naufalulhaq.13@gmail.com>
2024-01-01 23:17:20 +0700

    clean up unused files, integrate form and dashboard

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ mysql_config = {
     'database': 'tugas_akhir',
 }
 
+conn = mysql.connector.connect(**mysql_config)
+
 holiday_dict = {'08-10': 'Columbus Day',
                 '12-11': 'Veterans Day',
                 '22-11': 'Thanksgiving Day',
@@ -44,22 +46,75 @@ holiday_dict = {'08-10': 'Columbus Day',
                 '16-01': 'Martin Luther King Jr Day',
                 '20-02': 'Washingtons Birthday'}
 
-conn = mysql.connector.connect(**mysql_config)
-
 def Baseurl(address):
     return url_for('static', filename=address)
 
+def push_predict():
+    sql = "SELECT * FROM ( SELECT * FROM df_train_2 ORDER BY id DESC LIMIT 24 ) as SUBQUERY ORDER BY id"
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    myresult_prediction = cursor.fetchall()
+
+    df = pd.DataFrame(myresult_prediction)
+    df.drop(columns=['id'], inplace=True)
+
+    date_time = str(pd.to_datetime(df['date_time'].iloc[-1]) + pd.to_timedelta('1 hour'))
+    pred = int(predict(df))
+
+    insert_query = """INSERT INTO df_prediction_2 (date_time, prediction) 
+             VALUES (%s, %s)"""
+
+    cursor.execute(insert_query, (date_time, pred))
+    conn.commit()
+
+    cursor.close()
+
+    # return render_template('test.html', pred=pred, date_time=date_time)
+    return redirect(url_for('dashboard'))
+
 @app.route('/')
-def index():
-    return render_template('basic-table.html', baseurl=Baseurl)
+def dashboard():
+    sql = """SELECT date_time, traffic_volume, weather_description, weather_type, 
+            is_holiday, clouds_all, snow_p_h, rain_p_h, temperature, dew_point, 
+            visibility_in_miles, wind_direction, wind_speed, humidity, 
+            air_pollution_index FROM ( SELECT 
+                id, date_time, traffic_volume, weather_description, weather_type, 
+                is_holiday, clouds_all, snow_p_h, rain_p_h, temperature, dew_point, 
+                visibility_in_miles, wind_direction, wind_speed, humidity, 
+                air_pollution_index FROM df_train_2 ORDER BY id DESC LIMIT 24 ) 
+                as SUBQUERY ORDER BY id"""
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    myresult_train = cursor.fetchall()
+    cursor.close()
+
+    df_train = pd.DataFrame(myresult_train)
+    # df_train.drop(columns=['id'], inplace=True)
+    
+    sql = "SELECT * FROM ( SELECT * FROM df_prediction_2 ORDER BY id DESC LIMIT 25 ) as SUBQUERY ORDER BY id"
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    myresult_prediction = cursor.fetchall()
+    cursor.close()
+
+    df_prediction = pd.DataFrame(myresult_prediction)
+    df_prediction.drop(columns=['id'], inplace=True)
+
+    result_df = pd.merge(df_train, df_prediction, on='date_time', how='right')
+    result_df['date_time']=result_df['date_time'].str[:-6]
+
+    Results = {}
+    Results["date_time"] = result_df['date_time'].to_numpy().tolist()
+    Results["traffic_volume"] = result_df['traffic_volume'].to_numpy().tolist()
+    Results["prediction"] = result_df['prediction'].to_numpy().tolist()
+
+    return render_template('dashboard.html', baseurl=Baseurl, results=Results, data=myresult_train)
 
 @app.route('/form')
 def form():
     return render_template('form.html', baseurl=Baseurl)
-
-@app.route('/chart_coba')
-def chart_coba():
-    return render_template('chart.html', baseurl=Baseurl)
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
@@ -87,7 +142,7 @@ def process_data():
 
         # Insert data into the database
         insert_query = """
-            INSERT INTO sample 
+            INSERT INTO df_train_2 
             (date_time, is_holiday, air_pollution_index, humidity, wind_speed, wind_direction,
              visibility_in_miles, dew_point, temperature, rain_p_h, snow_p_h, clouds_all,
              weather_type, weather_description, traffic_volume)
@@ -105,112 +160,24 @@ def process_data():
         cursor.close()
 
         # Return a response to the client
-        return redirect(url_for('sql_table'))
+        # return redirect(url_for('push_predict'))
+        return push_predict()
         # return 'Data received and inserted into the database successfully'
 
 @app.route('/table')
 def table():
-    sql = "SELECT * FROM sample_v2"
+    sql = """SELECT date_time, traffic_volume, weather_description, weather_type, 
+             is_holiday, clouds_all, snow_p_h, rain_p_h, temperature, dew_point, 
+             visibility_in_miles, wind_direction, wind_speed, humidity, 
+             air_pollution_index FROM sample_v2"""
 
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    myresult = cursor.fetchall()
+
+    cursor.close()
 
     return render_template('table.html', baseurl=Baseurl, data=myresult)
-
-@app.route('/predict')
-def predict_data():
-    sql = "SELECT * FROM ( SELECT * FROM sample_v2 ORDER BY id DESC LIMIT 24 ) as SUBQUERY ORDER BY id"
-
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-
-    df = pd.DataFrame(myresult)
-    df.drop(columns=['id'], inplace=True)
-    
-    pred = predict(df)
-    print(pred)
-
-    return render_template('pred.html', baseurl=Baseurl, data=myresult, pred=pred)
-
-@app.route('/chart')
-def chart():
-    sql = "SELECT * FROM sample"
-
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-
-    Results = {}
-    Results["date_time"] = [entry["date_time"] for entry in myresult]
-    Results["traffic_volume"] = [entry["traffic_volume"] for entry in myresult]
-
-    return render_template('chart.html', baseurl=Baseurl, results=Results)
-
-@app.route('/chart_2')
-def chart_2():
-    sql = "SELECT * FROM ( SELECT * FROM second_half ORDER BY id DESC LIMIT 24 ) as SUBQUERY ORDER BY id"
-
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-
-    df = pd.DataFrame(myresult)
-    df.drop(columns=['id'], inplace=True)
-    
-    pred = int(predict(df)[0].round())
-
-    Results = {}
-    Results["date_time"] = [entry["date_time"] for entry in myresult]
-    Results["traffic_volume"] = [entry["traffic_volume"] for entry in myresult]
-
-    return render_template('chart_2.html', baseurl=Baseurl, results=Results, pred=pred, data=myresult)
-
-@app.route('/chart_3')
-def chart_3():
-    sql = "SELECT * FROM ( SELECT * FROM df_train ORDER BY id DESC LIMIT 24 ) as SUBQUERY ORDER BY id"
-
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult_train = mycursor.fetchall()
-
-    df_train = pd.DataFrame(myresult_train)
-    df_train.drop(columns=['id'], inplace=True)
-    
-    sql = "SELECT * FROM ( SELECT * FROM df_prediction ORDER BY id DESC LIMIT 25 ) as SUBQUERY ORDER BY id"
-
-    mycursor = conn.cursor(dictionary=True)
-    mycursor.execute(sql)
-    myresult_prediction = mycursor.fetchall()
-
-    df_prediction = pd.DataFrame(myresult_prediction)
-    df_prediction.drop(columns=['id'], inplace=True)
-
-    # Columns to be dropped
-    columns_to_drop = ['is_holiday', 'air_pollution_index', 'humidity', 'wind_speed',
-                       'wind_direction', 'visibility_in_miles', 'dew_point', 'temperature',
-                       'rain_p_h', 'snow_p_h', 'clouds_all', 'weather_type', 'weather_description']
-
-    # Tail for the last 24 rows
-    df_train_ = df_train.drop(columns=columns_to_drop).tail(24)
-    df_prediction_ = df_prediction.tail(25)
-
-    result_df = pd.merge(df_train_, df_prediction_, on='date_time', how='right')
-    result_df.info()
-
-    pred = int(predict(df_train)[0].round())
-
-    Results = {}
-    # Results["date_time"] = [entry["date_time"] for entry in myresult_train]
-    # Results["traffic_volume"] = [entry["traffic_volume"] for entry in myresult_train]
-    Results["date_time"] = result_df['date_time'].to_numpy().tolist()
-    Results["traffic_volume"] = result_df['traffic_volume'].to_numpy().tolist()
-    Results["prediction"] = result_df['prediction'].to_numpy().tolist()
-    print(type(Results))
-    print(Results)
-
-    return render_template('chart_3.html', baseurl=Baseurl, results=Results, pred=pred, data=myresult_train)
 
 if __name__ == '__main__':
     app.run(debug='True')
